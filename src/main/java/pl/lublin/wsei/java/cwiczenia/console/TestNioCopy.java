77cb3f96df2b8c7b0f2c07edc8c180ffcb3acdda
Nahoris <albertjedrych@gmail.com>
2022-06-05 15:22:34 +0200

    Lab3a

--- src/main/java/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
@@ -1,19 +1,19 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+
 public class TestNioCopy {
     public static void main(String[] args) throws IOException {
         //long starTime = System.currentTimeMillis();
         long startTime = -System.nanoTime();//dokładniejszy pomiar
-        Path oryginal = new Path("sample_1920x1280.tiff");
-        Path kopia = new File("img_copy2.tiff");
-        Files.copy(oryginal.toAbsolutePath(), kopia.toAbsolutePath());
+        Path oryginal = Paths.get("sample_1920x1280.tiff");
+        Path kopia = Paths.get("img_copy2.tiff");
+        Files.copy(oryginal, kopia, StandardCopyOption.REPLACE_EXISTING);
         // System.out.println("Czas kopiowa: " + (System.currentTimeMillis() - startTime)+" ms");
         System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000+" ms");
     }
